87189a8c287a1b6e90a51bd24b20044a3a3f6ae2
AZ2Agent <woob19473@gmail.com>
2025-08-17 22:01:05 +0700

    ft : handle logic send mail

--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -1,10 +1,34 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
-
 @Module({
-  imports: [],
-  controllers: [NotificationController],
-  providers: [NotificationService],
+    imports: [
+        MailerModule.forRootAsync({
+            useFactory: () => ({
+                transport: {
+                    host: process.env.SMTP_HOST,
+                    port: +process.env.SMTP_PORT,
+                    secure: false,
+                    tls: {
+                        rejectUnauthorized: false,
+                    },
+                },
+                defaults: {
+                    from: process.env.FROM,
+                },
+                template: {
+                    dir: __dirname + '/../../templates',
+                    adapter: new PugAdapter(),
+                    options: {
+                        strict: true,
+                    },
+                },
+            }),
+        }),
+    ],
+    controllers: [NotificationController],
+    providers: [NotificationService],
 })
-export class NotificationModule {}
+export class NotificationModule { }
--- apps/notification/src/notification.service.ts
+++ apps/notification/src/notification.service.ts
@@ -1,8 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ISendMailOptions, MailerService } from '@nestjs-modules/mailer';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class NotificationService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    private readonly logger = new Logger(NotificationService.name);
+
+    constructor(private readonly mailerService: MailerService) { }
+
+    async sendEmail(params: {
+        subject: string;
+        template: string;
+        context: ISendMailOptions['context'];
+    }) {
+        try {
+            const emailsList: string[] = process.env.SMTP_TO?.split(',');
+
+            if (!emailsList) {
+                throw new BadRequestException(
+                    `No recipients found in SMTP_TO env var, please check your .env file`,
+                );
+            }
+
+            const sendMailParams = {
+                to: emailsList,
+                from: process.env.SMTP_FROM,
+                subject: params.subject,
+                template: params.template,
+                context: params.context,
+            };
+            const response = await this.mailerService.sendMail(sendMailParams);
+            this.logger.log(
+                `Email sent successfully to recipients with the following parameters : ${JSON.stringify(
+                    sendMailParams,
+                )}`,
+                response,
+            );
+        } catch (error) {
+            this.logger.error(
+                `Error while sending mail with the following parameters : ${JSON.stringify(
+                    params,
+                )}`,
+                error,
+            );
+        }
+    }
 }
