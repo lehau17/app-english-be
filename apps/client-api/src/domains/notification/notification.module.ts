69916ada6cd0e75ee1fd4f9691d437234a4f40bb
lehau17 <hau17131203@gmail.com>
2025-11-05 14:21:21 +0700

    refactor(notification): Improve NotificationModule structure and export NotificationService
    - Reformatted NotificationModule for consistent indentation and readability.
    - Added export for NotificationService to allow injection in other modules.

--- apps/client-api/src/domains/notification/notification.module.ts
+++ apps/client-api/src/domains/notification/notification.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrivateNotificationController } from './controller';
-import { NotificationService } from './service';
 import { NotificationRepository } from './repository';
+import { NotificationService } from './service';
 
 @Module({
-  controllers: [PrivateNotificationController],
-  providers: [NotificationService, NotificationRepository],
+    controllers: [PrivateNotificationController],
+    providers: [NotificationService, NotificationRepository],
+    exports: [NotificationService], // Export để các module khác có thể inject
 })
-export class NotificationModule {}
+export class NotificationModule { }
